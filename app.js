75d02f93d1f95ffa6ec1f0c566234919ba81294e
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-02 15:48:12 +0100

    1.0

---
+++ app.js
@@ -0,0 +1,16 @@
+let paragraphe = document.getElementsByTagName("p");
+
+function chiffreRandom(){
+
+    return Math.floor(Math.random()*100);
+
+}
+
+console.log(chiffreRandom());
+
+for(let i=0;i<paragraphe.length;i++){
+    chiffreRandom();
+    paragraphe[i].innerHTML= chiffreRandom().toString();
+}
+
+console.log(paragraphe);
